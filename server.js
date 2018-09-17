94d790b89f74da81d725258350c5d3b593661e3a
unknown <Tanawat Monpipat>
2018-09-17 16:43:22 +0700

    studentserver

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var pgp = require('pg-promise')();
 var app = express();
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+  host     : 'www.db4free.net',
+  user     : 's140390',
+  password : 'abc123**',
+  database : 'db140390'
+});
+
+
+
 
 //app.use(express.static ('static') );
 app.set('view engine', 'ejs');
@@ -8,10 +19,19 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
     res.render('page/home');
         
-    
+
     });
 
+//connection
+connection.connect()
 
+connection.query('SELECT * from students', function (err, rows, fields) {
+    if (err) throw err
+
+  console.log(rows)
+})
+
+connection.end()
 
 
 console.log('Appp is running at http://localhost:8080');          
